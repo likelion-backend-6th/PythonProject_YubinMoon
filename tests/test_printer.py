d57b6cbda10ab86847ce2fdd231612aa02c8efe0
terry <munu220@gmail.com>
2023-07-09 02:53:19 +0900

    testcase 추가

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -49,3 +49,26 @@ def test_print_no_select(mocker):
 
     num_list = [d.find("||") for d in data_list]
     assert len(set(num_list))
+
+
+def test_print_no_select(mocker):
+    test = TestClass()
+    mocker.patch("os.get_terminal_size", return_value=TestClass)
+    printer = Printer()
+    render_data = RenderData(
+        menu_list=["abc", "ccc", "bdc"], detail_data="testDetail\ntesttest"
+    )
+    result = printer.render(render_data)
+    assert "abc" in result
+    assert "ccc" in result
+    assert "bdc" in result
+    assert "testDetail" in result
+    assert ">" not in result
+
+    data_list = result.split("\n")
+    assert len(data_list) == test.lines
+    for data in data_list:
+        assert len(data) <= test.columns
+
+    num_list = [d.find("||") for d in data_list]
+    assert len(set(num_list))
