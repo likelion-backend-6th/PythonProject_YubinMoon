daa9f4c1261e750863a79363cafc46d28ccc8d2b
terry <munu220@gmail.com>
2023-07-09 23:14:07 +0900

    BaseMenuPage 생성
    mainPage 랑 NewbooksPage랑 목록 구현 부분이 중복됨

--- main.py
+++ main.py
@@ -106,7 +106,54 @@ class BasePage:
         raise NotImplementedError
 
 
-class MainPage(BasePage):
+class BaseMenuPage(BasePage):
+    def __init__(self):
+        super().__init__()
+        self.menu_list: list[list[str]] = []
+        self.selected = -1
+        self.detail = ""
+
+    def get_render_data(self) -> RenderData:
+        return RenderData(
+            menu_list=[name for name, _ in self.menu_list],
+            select_data=self.selected,
+            detail_data=self.detail,
+        )
+
+    def run(self, key: str) -> str | None:
+        key = key.lower()
+        self.move_menu(key)
+        if key == "h":
+            return "help"
+        elif key == "q":
+            return "exit"
+        elif key == "b":
+            return "back"
+        elif key == "enter":
+            if self.selected is not None and 0 <= self.selected < len(self.menu_list):
+                return self.menu_list[self.selected][1]
+
+    def move_menu(self, key: str) -> None:
+        key = key.lower()
+        if key == "k":
+            if self.selected is None:
+                self.selected = 0
+            elif 0 < self.selected:
+                self.selected -= 1
+            else:
+                self.selected = len(self.menu_list) - 1
+        elif key == "j":
+            if self.selected is None:
+                self.selected = 0
+            elif self.selected < len(self.menu_list) - 1:
+                self.selected += 1
+            else:
+                self.selected = 0
+        elif key == "esc":
+            self.selected = -1
+
+
+class MainPage(BaseMenuPage):
     def __init__(self):
         super().__init__()
         self.menu_list = [
@@ -120,41 +167,8 @@ this page is main
 made by yubin
 press "h" to help"""
 
-    def get_render_data(self) -> RenderData:
-        return RenderData(
-            menu_list=[name for name, _ in self.menu_list],
-            select_data=self.selected,
-            detail_data=self.detail,
-        )
 
-    def run(self, key: str) -> str | None:
-        key = key.lower()
-        if key == "k":
-            if self.selected is None:
-                self.selected = 0
-            elif 0 < self.selected:
-                self.selected -= 1
-            else:
-                self.selected = len(self.menu_list) - 1
-        elif key == "j":
-            if self.selected is None:
-                self.selected = 0
-            elif self.selected < len(self.menu_list) - 1:
-                self.selected += 1
-            else:
-                self.selected = 0
-        elif key == "h":
-            return "help"
-        elif key == "q":
-            return "exit"
-        elif key == "enter":
-            if self.selected is not None and 0 <= self.selected < len(self.menu_list):
-                return self.menu_list[self.selected][1]
-        elif key == "esc":
-            self.selected = -1
-
-
-class NewBooksPage(BasePage):
+class NewBooksPage(BaseMenuPage):
     def __init__(self):
         super().__init__()
         self.menu_list = [
@@ -163,37 +177,6 @@ class NewBooksPage(BasePage):
         ]
         self.selected = -1
         self.detail = "asdf"
-
-    def get_render_data(self) -> RenderData:
-        return RenderData(
-            menu_list=[name for name, _ in self.menu_list],
-            select_data=self.selected,
-            detail_data=self.detail,
-        )
-
-    def run(self, key: str) -> str | None:
-        key = key.lower()
-        if key == "k":
-            if self.selected is None:
-                self.selected = 0
-            elif 0 < self.selected:
-                self.selected -= 1
-            else:
-                self.selected = len(self.menu_list) - 1
-        elif key == "j":
-            if self.selected is None:
-                self.selected = 0
-            elif self.selected < len(self.menu_list) - 1:
-                self.selected += 1
-            else:
-                self.selected = 0
-        elif key == "q":
-            return "exit"
-        elif key == "enter":
-            if self.selected is not None and 0 <= self.selected < len(self.menu_list):
-                return self.menu_list[self.selected][1]
-        elif key == "esc":
-            self.selected = -1
 
 
 class Controller:
