ff2ebc35364af8af966ab41970d3351766a676ae
terry <munu220@gmail.com>
2023-07-09 23:39:28 +0900

    controller testcase 추가

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,8 +1,27 @@
-from main import MainPage, Controller
+from main import MainPage, Controller, NewBooksPage
 
 
 def test_get_page(mocker):
     mocker.patch("main.Printer", return_value="test")
     controller = Controller()
-    menu = controller.get_page("main")
-    assert isinstance(menu, MainPage)
+    controller.get_page("main")
+    assert isinstance(controller.page, MainPage)
+    controller.get_page("new_books")
+    assert isinstance(controller.page, NewBooksPage)
+
+
+def test_handle_event(mocker):
+    mocker.patch("main.Printer", return_value="test")
+    mocker.patch("main.Controller.exit", return_value=None)
+    mocker.patch("main.Controller.get_page", return_value=None)
+    controller = Controller()
+    controller.handle_event("test1")
+    controller.get_page.assert_called_with("test1")
+    assert controller.page_stack == ["main", "test1"]
+    controller.handle_event("back")
+    controller.get_page.assert_called_with("main")
+    assert controller.page_stack == ["main"]
+    controller.handle_event("back")
+    assert controller.page_stack == ["main"]
+    controller.handle_event("exit")
+    controller.exit.assert_called_once()
